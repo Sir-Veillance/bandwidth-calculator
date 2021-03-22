d0da9029936d90807259be809ce94d3396fd43d4
rachaelrogan <47510726+rachaelrogan@users.noreply.github.com>
2021-03-22 16:42:23 -0400

    3/22
    Created example.txt file, read from file, created nodes and connections @Sir-Veillance

--- Node.cs
+++ Node.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 
 namespace bandwidth_calculator {
     class Node {
-        public Node(string id, int output) {
-            string nodeId = id;
-            List<Node> connections = new List<Node>();
-            int maxOutput = output;
-            int availableOutput = maxOutput; // something we determine from recursing backward
+        private int _nodeId;
+        public List<(Node, int, int)> connections {get;}
+        public Node(int id) {
+            _nodeId = id;
+            connections = new List<(Node, int, int)>(); // (node, bandwidth, used)
         }
-        public string nodeId {get;}
-        public List<Node> connections {get;}
-        public int maxOuput {get;set;}
-        public int availableOutput {get;set;}
-        
-        public void addConnection(Node newNode) {
-            connections.Add(newNode);
+        public int nodeId {
+            get {
+                return _nodeId;
+            }
+        }
+        public void addConnection(Node newNode, int bandwidth, int used) {
+            connections.Add((newNode, bandwidth, used));
         }
 
         // Look through the connection list of both nodes and return a list of shared nodes.
         // May need to do this recursively to see if there are shared nodes further back
         public List<Node> getSharedConnections(Node node1, Node node2) {
             return new List<Node>();
+        }
+        public override string ToString(){
+            string outString = new String("nodeId:" + nodeId);
+            outString += "\nConnections:\n";
+            foreach (var connection in connections) {
+                outString += "Node: " + connection.Item1.nodeId + " Bandwidth: " + connection.Item2 + " Used: " + connection.Item3 + "\n";
+            }
+            return outString;
         }
 
         // Look at all shared Nodes and pull from the node that can give the maximum value.
--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+//using bandwidth_calculator.Node;
+
+/*
+-Find every path from source to destination without cycles (not the same node twice in the path)
+-Sort paths by length (same length paths are arbitrarily sorted)
+-Send the maximum "bandwidth" from source to destination, and track leftover bandwidth at each node
+-Go through the paths in sorted order doing this, if a path does not have any leftover bandwidth from source or from any node along the path, it is eliminated
+-Solution should be total amount of bandwidth that reached destination after going through every path/eliminating every path
+*/
 
 namespace bandwidth_calculator
 {
@@ -7,17 +18,37 @@ namespace bandwidth_calculator
     {
         static void Main(string[] args)
         {
+            // file parsing
             Console.WriteLine("Please enter a filename with extension: ");
             string filename = Console.ReadLine();
             if (File.Exists(filename)) {
                 string[] input = File.ReadAllLines(filename);
+                int nodeCount = int.Parse(input[0]);
+                List<Node> nodeList = new List<Node>();
+                for (int i = 1; i < nodeCount + 1; i++) {
+                    nodeList.Add(new Node(i));
+                }
                 string[] sourceAndDestination = input[1].Split(' ');
-                string source = sourceAndDestination[0];
-                string destination = sourceAndDestination[1];
-                
+                int sourceID = int.Parse(sourceAndDestination[0]);
+                int destinationID = int.Parse(sourceAndDestination[1]);
+                for (int i = 2; i < input.Length - 1; i++) {
+                    string[] parsedLine = input[i].Split(' ');
+                    Node firstNode = nodeList.Find(node => node.nodeId == int.Parse(parsedLine[0]));
+                    Node secondNode = nodeList.Find(node => node.nodeId == int.Parse(parsedLine[1]));
+                    int connectionBandwidth = int.Parse(parsedLine[2]);
+                    firstNode.addConnection(secondNode, connectionBandwidth, 0);
+                    secondNode.addConnection(firstNode, connectionBandwidth, 0);
+                }
+                foreach (var node in nodeList){
+                    Console.WriteLine(node);
+                }
+
             } else {
                 Console.WriteLine("Invalid filename...");
             }
+
+            // path generation
+            
         }
     }
 }
